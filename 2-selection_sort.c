a3ff74bf5fdf7f33485153f1bb25cad7f4afcadc
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-04-21 09:32:40 -0700

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,12 +11,13 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int index, min;
+	int index, min, flag;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
+		flag = 0;
 		min = array[i];
 		index = i;
 		for (j = i + 1; j < size; j++)
@@ -25,11 +26,15 @@ void selection_sort(int *array, size_t size)
 			{
 				min = array[j];
 				index = j;
+				flag = 1;
 			}
 		}
-		array[index] = array[i];
-		array[i] = min;
-		print_array(array, size);
+		if (flag > 0)
+		{
+			array[index] = array[i];
+			array[i] = min;
+			print_array(array, size);
+		}
 	}
 
 
