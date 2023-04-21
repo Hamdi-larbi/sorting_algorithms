27d324882e3ef4391fe3644efcd74cccf96318f0
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-04-21 08:58:25 -0700

    select sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: the array to be sorted
+ * @size: the length of the array
+ * return nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int index, min;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		array[index] = array[i];
+		array[i] = min;
+		print_array(array, size);
+	}
+
+
+
+}
