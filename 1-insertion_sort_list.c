d466cf0b1db849356e9263a19d2b1ffcbf770897
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-04-22 15:17:13 -0700

    update insertion sort algorithme

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *node, *ptr;
 
-	if (*list == NULL || (*list)->next == NULL)
+/*	if (*list == NULL || (*list)->next == NULL)*/
+	if (*list == NULL)
 		return;
 	for (node = *list; node != NULL; node = node->next)
 	{
